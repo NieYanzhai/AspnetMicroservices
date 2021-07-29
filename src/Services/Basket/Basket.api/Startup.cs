544b399a5e22630a4ec57d81c9ac81e4f705faf1
nieyanzhai <yanzhai.nie@outlook.com>
2021-07-29 11:55:04 +0800

    Add Basket.Api

--- src/Services/Basket/Basket.api/Startup.cs
+++ src/Services/Basket/Basket.api/Startup.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace Basket.api
+namespace Basket.Api
 {
     public class Startup
     {
@@ -30,7 +30,7 @@ namespace Basket.api
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.api", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.Api", Version = "v1" });
             });
         }
 
@@ -41,7 +41,7 @@ namespace Basket.api
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.api v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.Api v1"));
             }
 
             app.UseHttpsRedirection();
